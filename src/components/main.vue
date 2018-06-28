3ac4b4d56bc39eeba5c5e3d3f431ce56576496d6
wangkeshan9538 <wangkeshan9538@gmail.com>
2018-06-28 23:17:03 +0800

    写了一个布局，好难。。。

---
+++ src/components/main.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-container>
+      <el-header><head></head></el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+</template>
+
+<script>
+  import head from "@/components/head"
+
+export default {
+  name: 'main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  comments:{
+    head
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-container{
+    height: 100%;
+    width: 100%;
+
+  }
+</style>
