8d15ce22f4c4466ecce165533b1f6c0fe2dc7f59
wangkeshan9538 <wangkeshan9538@gmail.com>
2018-07-01 23:24:35 +0800

    head 的样式和布局

---
+++ src/components/headSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="head_section">
+  <el-row>
+    <el-col id="logo" :span="2">1</el-col>
+    <el-col :span="22">1</el-col>
+  </el-row>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "head-section"
+    }
+</script>
+
+
+<style scoped>
+ #head_section{
+   height: 100%;
+   width: 100%;
+   background-color: aqua;
+ }
+ .el-row{
+   height: 100%;
+ }
+  #logo{
+    background: antiquewhite;
+    height: 100%;
+  }
+</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,34 +1,43 @@
 <template>
-    <el-container>
-      <el-header><head></head></el-header>
+    <el-container id="main_section">
+
+      <el-header height="6%">
+        <head-section></head-section>
+      </el-header>
+
       <el-container>
         <el-aside width="200px">Aside</el-aside>
         <el-main>Main</el-main>
       </el-container>
+
     </el-container>
 </template>
 
 <script>
-  import head from "@/components/head"
+  import HeadSection from "./headSection";
 
-export default {
-  name: 'main',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
-  comments:{
-    head
+  export default {
+    components: {HeadSection},
+    name: 'mainSection',
+    /*data () {
+      return {
+
+      }
+    },*/
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .el-container{
+  #main_section{
     height: 100%;
     width: 100%;
+  }
+  .el-container{
 
+  }
+  .el-header{
+    padding:0px;
+    min-height:60px;
   }
 </style>
