d078da69f7801be78e73e3f065d612e3d1b1160c
wangkeshan9538 <wangkeshan9538@gmail.com>
2018-09-21 09:00:18 +0800

    完成quartzContext

--- quarzService/src/main/java/com/wks/quartzService/jobs/Task1.java
+++ quarzService/src/main/java/com/wks/quartzService/jobs/Task1.java
@@ -2,6 +2,9 @@ package com.wks.quartzService.jobs;
 
 import org.quartz.*;
 import org.quartz.impl.matchers.GroupMatcher;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import javax.annotation.Resource;
@@ -10,65 +13,10 @@ import java.util.List;
 
 public class Task1 extends QuartzJobBean {
 
-
-  public int v;
-
-  @Resource(name="executeServ")
-  private ExecuteServ executeServ;
-
-  public void setExecuteServ(ExecuteServ executeServ) {
-    this.executeServ = executeServ;
-  }
-
   @Override
   protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
 
     System.out.printf("Task执行");
 
-    System.out.printf("  "+executeServ.m+"  "+v);
-
-    JobDetail jd=context.getJobDetail();
-
-    //停止任务
-    try {
-      context.getScheduler().deleteJob(jd.getKey());
-      //pauseJob rescheduleJob  delete
-
-      //Pause the <code>{@link org.quartz.JobDetail}</code> with the given
-      //key - by pausing all of its current <code>Trigger</code>s.
-      context.getScheduler().pauseJob(jd.getKey()); //pause Job
-
-      //Remove the indicated <code>{@link Trigger}</code> from the scheduler.
-      //If the related job does not have any other triggers, and the job is
-      //not durable, then the job will also be deleted.</p>
-      context.getScheduler().unscheduleJob(context.getTrigger().getKey()); //remove all trigger
-
-      //Delete the identified <code>Job</code> from the Scheduler - and any
-      //     * associated <code>Trigger</code>s.
-      context.getScheduler().deleteJob(jd.getKey()); //removeJob
-
-      List<String> groups=context.getScheduler().getJobGroupNames();
-
-      for(String s:groups){
-        GroupMatcher<TriggerKey> triggerKeyGroupMatcher =GroupMatcher.groupEquals(s)  ;
-        context.getScheduler().getTriggerKeys(triggerKeyGroupMatcher).iterator().next();
-//        context.getScheduler().getTriggersOfJob();
-      }
-
-    } catch (SchedulerException e) {
-      e.printStackTrace();
-    }
-
-    //增加job
-    //context.getScheduler().addJob("");
-    this.v++;
-
-    //开启任务
-
-    //修改任务时间
-
-    //获得任务列表 及状态
-
-    //立即触发
   }
 }
--- quarzService/src/main/java/com/wks/quartzService/problem.md
+++ quarzService/src/main/java/com/wks/quartzService/problem.md
@@ -2,3 +2,10 @@
 2. 通过在job中带一个属性变量v  且每次++ 可以发现 每次都是new job 出的实例， 而不是单例
 3. forJob() 方法 只是提取jobKey ，并不和实例有关，
 4. pauseJob 暂停job unscheduleJob 停用 trigger
+
+5. quartz 在 初始化 脚本执行的时候 如果不指定 comment-prefix 那么脚本会执行错误 ，
+ ```apple js
+spring.quartz.jdbc.comment-prefix=#
+```
+
+6. 把 quartz中执行 sql 脚本的代码抽出来
